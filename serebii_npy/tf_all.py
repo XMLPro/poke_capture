d537e3609f6708e9cae2848cbac5a6513c50ade1
ctare <sankaku900622@yahoo.co.jp>
2017-11-22 11:06:11 +0900

    bug fix and 67500

--- serebii_npy/tf_all.py
+++ serebii_npy/tf_all.py
@@ -10,35 +10,35 @@ img = tf_rect.readimg(233)
 cnt = 0
 tfimgs = []
 
-# yellow rect : 3 * 5 * 5
+# yellow rect : 3 * 2 * 2
 for size in trange(3, 6, desc="rect s"):
-    for y in trange(16, 26, 2, desc="rect y"):
-        for x in trange(16, 26, 2, desc="rect x"):
+    for y in trange(16, 20, 3, desc="rect y"):
+        for x in trange(16, 20, 3, desc="rect x"):
             rect_img = tf_rect.rect(img, x, y, size, size, (255, 255, 0))
             mask_img = (np.sum(rect_img, axis=2) != 0) * 255
             mask_img = cv2.merge((mask_img, mask_img, mask_img))
             mask_img = mask_img.astype(np.uint8)
 
-            # rgb and hls : 3 * 5 * 5
+            # rgb and hls : 3 * 5 * 3
             for rgb in trange(3, desc="rgb"):
-                for i in trange(20, 100, 20, desc="col"):
-                    for j in range(-80, 20, 20):
-                        cimg = tf_color.color(rgb, rect_img, i, mask_img)
-                        cimg = tf_color.hls(cimg, j, mask_img)
+                for i in trange(20, 81, 20, desc="col"):
+                    cimg = tf_color.color(rgb, rect_img, i, mask_img)
+                    for j in range(-60, 1, 30):
+                        himg = tf_color.hls(cimg, j, mask_img)
 
                         # to 100x100
-                        cimg = cv2.resize(cimg, (100, 100))
+                        himg = cv2.resize(himg, (100, 100))
 
-                        # affine : 8 * 8 * 10
-                        for vr in range(0, 40, 5):
-                            for vl in range(0, 40, 5):
-                                for top in range(0, 50, 5):
+                        # affine : 5 * 5 * 5
+                        for vr in range(0, 41, 10):
+                            for vl in range(0, 41, 10):
+                                for top in range(0, 41, 10):
                                     p2 = np.float32([
                                         [50, i],
                                         [80 - 20 + vr, 80],
                                         [20 + 20 - vl, 80]
                                         ])
-                                    amig = tf_affine.affine(cimg, p2)
+                                    amig = tf_affine.affine(himg, p2)
                                     tfimgs.append(amig)
 
 result = np.array(tfimgs)
